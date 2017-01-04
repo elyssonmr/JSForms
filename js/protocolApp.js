0f15878e4de6795add75ab703436ef2617656fae
Elysson MR <elyssonmr@gmail.com>
2017-01-04 12:00:04 -0200

    Adiciona o ngRoute ao projeto

--- js/protocolApp.js
+++ js/protocolApp.js
@@ -1,15 +1,15 @@
 /*
  * AngularJS Module
  */
-var protocolApp = angular.module('protocolApp', []);
+var protocolApp = angular.module("protocolApp", ["ngRoute"]);
 
 protocolApp.run(function($rootScope) {
-    $rootScope.pageTitle = '';
+    $rootScope.pageTitle = "";
     $rootScope.setTitle = function(title) {
         if(title) {
-            $rootScope.pageTitle = '- ' + title;
+            $rootScope.pageTitle = "- " + title;
         } else {
-            $rootScope.pageTitle = '';
+            $rootScope.pageTitle = "";
         }
     };
 });
